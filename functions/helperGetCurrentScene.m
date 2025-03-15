eb1ffd30ec110dc7ace96bcedf2a9d17964f83a3
FilippoOttaviani <filippo.ottaviani.tr@gmail.com>
2025-03-15 12:05:30 +0100

    Aggiornato l'algoritmo di visualizzazione della nuvola di punti animata

---
+++ functions/helperGetCurrentScene.m
@@ -0,0 +1,10 @@
+function sceneName = helperGetCurrentScene()
+    %helperGetCurrentScene Get name of current scene
+    %   sceneName = helperGetCurrentScene() returns the name of the current
+    %   scene selected in the simulation.
+    
+    % Copyright 2019 The MathWorks, Inc.
+    
+    % For this example, the scene name is hardcoded
+    sceneName = 'LargeParkingLot';
+end
